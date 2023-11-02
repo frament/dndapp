1563160c65d4f7f439f576696134d240a32fac03
Peter Shuvalov <frament@mail.ru>
2023-11-02 20:40:10 +0500

    *

--- client/src/app/auth/Auth.component.ts
+++ client/src/app/auth/Auth.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UserService} from "../user/user.service";
 import {Router} from "@angular/router";
--- client/src/app/auth/auth.guard.ts
+++ client/src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {UserService} from "../user/user.service";
 
-export const authGuard: CanActivateFn = async (route, state) : Promise<boolean> => {
+export const authGuard: CanActivateFn = async () : Promise<boolean> => {
   const service = inject(UserService);
   const router = inject(Router);
   if (service.user) return true;
